42b25ca876b26fdac5bc86f44e64a026a9b287e7
Tu Nguyen <tunguyen211098@gmail.com>
2022-08-03 23:56:18 +0700

    Final test completed

--- Excercise-1/index1.js
+++ Excercise-1/index1.js
@@ -1,35 +1,44 @@
 // Tạo một ứng dụng web, cho phép nhập vào 2 số a và b
-// Hiển thị tất cả các số nguyên tố trong khoảng a và b.  Nhập trên HTM
-let a = Number(prompt("nhap so thu nhat:", ""));
-let b = Number(prompt("nhap so thu nhat:", ""));
+// Hiển thị tất cả các số nguyên tố trong khoảng a và b.  Nhập trên HTML
 
-let start, end;
-if (a < 0 && b < 0) {
-  document.write("Ko co so nguyen to nao < 0 ");
-  console.log("Ko co so nguyen to nao < 0 ");
-} else if (a > b && b < 0) {
-  end = a;
-  start = 1;
-} else if (a > b && a > 0 && b > 0) {
-  end = a;
-  start = b;
-} else if (b > a && a > 0 && b > 0) {
-  start = a;
-  end = b;
-} else if (b > a && a < 0) {
-  end = b;
-  start = 1;
-}
-
-document.write(`So nguyen to tu ${start} den ${end} la: `, "<br>");
-for (let i = start; i <= end; i++) {
-  smt = 0;
-  for (j = 2; j <= i / 2; j++) {
-    if (i % j == 0) {
-      smt++;
+// Function kiem tra so nguyen to:
+const checkSNT = (number) => {
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      count++;
     }
   }
-  if ((smt == 0 || i == 2) && i != 1) {
-    document.write(i, ", ");
+  if (count === 2) {
+    return true;
+  } else {
+    return false;
   }
-}
+};
+
+document.getElementById('run').onclick = () => {
+  const a = parseInt(document.getElementById('aNumber').value);
+  const b = parseInt(document.getElementById('bNumber').value);
+  const result = document.getElementById('result');
+
+  const listNumber = [];
+  if (a <= b) {
+    for (let i = a; i <= b; i++) {
+      if (checkSNT(i) === true) {
+        listNumber.push(i);
+      }
+    }
+
+    if (listNumber.length === 0) {
+      result.innerHTML = `<p>Không có số nguyên tố trong khoảng ${a} -> ${b}</p>`;
+    } else {
+      let stringHTML = '';
+      for (let number of listNumber) {
+        stringHTML += `<p>${number}</p>`;
+      }
+      result.innerHTML = stringHTML;
+    }
+  } else {
+    result.innerHTML = `<p> a < b is required </p>`;
+  }
+};
